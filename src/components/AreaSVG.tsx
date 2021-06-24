7301ccc3ff02d43854e591a52b58c92f027356a6
defunty <tkqrt4131@gmail.com>
2021-06-24 21:14:15 +0900

    [Add] map生成機能追加

--- src/components/AreaSVG.tsx
+++ src/components/AreaSVG.tsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect }from 'react'
 import styled from '@emotion/styled'
 
-const AreaSVG = () => {
+type Props = {
+  number: number,
+  resource: string
+}
+const AreaSVG = ({ number, resource }: Props) => {
   return (
-    <StyledRoot>
+    <StyledRoot className={resource}>
       <svg viewBox="-150 -150 300 300">
         <path d="M138 57 L 57 138 L -57 138 L -138 57 L -138 -57 L -57 -138 L 57 -138 L 138 -57 z"></path>
       </svg>
+      <div className="Number">{number}</div>
     </StyledRoot>
   )
 }
 
 const StyledRoot = styled.div`
+  position: relative;
   height: 60px;
   width: 60px;
   svg {
@@ -19,8 +25,35 @@ const StyledRoot = styled.div`
     width: 100%;
     height: 100%;
   }
-  path {
-    fill: red;
+
+  &.brick path {
+    fill: brown;
+  }
+  &.grain path {
+    fill: gold;
+  }
+  &.ore path {
+    fill: silver;
+  }
+  &.wool path {
+    fill: lawngreen;
+  }
+  &.wood path {
+    fill: darkgreen;
+  }
+  &.desert path {
+    fill: yellow;
+  }
+  .Number {
+    background-color: #fff;
+    color: #333;
+    text-aligin: center;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    border-radius: 20px;
+    height: 20px;
+    width: 20px;
   }
 `
 
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,27 +2,100 @@ import React, { useState, useEffect }from 'react'
 import styled from '@emotion/styled'
 import AreaSVG from './AreaSVG'
 
+type Field = {resource: string, number: number}
+
 const Map = () => {
+  const [fields, setFields] = useState<Field[]>([]);
+
+  const clickRegenerateButton = () => {
+    getRandomFields().then(randomFields => {
+      setFields(randomFields)
+    });
+  }
+
+  async function getRandomFields(): Promise<Field[]> {
+    const randomise = (array: any[]) => {
+      const newArray = [];
+      while (array.length > 0) {
+        const n = array.length;
+        const k = Math.floor(Math.random() * n);
+
+        newArray.push(array[k]);
+        array.splice(k, 1);
+      }
+      return newArray;
+    }
+
+    const resourceArray = [
+      'brick','brick','brick',
+      'ore','ore','ore',
+      'grain','grain','grain','grain',
+      'wood','wood','wood','wood',
+      'wool','wool','wool','wool',
+      'desert'
+    ]
+    const numberArray = [0,2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
+    const randomResourceArray = randomise(resourceArray)
+    const randomNumberArray = randomise(numberArray)
+
+    const proc = (randomResourceArray: string[], randomNumberArray: number[]) => {
+      const randomFields: Field[] = []
+
+      let nullIndex: number = 0
+      let nullResource: string = ''
+      let desertIndex: number = 0
+      let desertNumber: number = 0
+      for(let i = 0; i<randomNumberArray.length; i++) {
+        if (randomNumberArray[i] === 0) {
+          nullIndex = i
+          nullResource = randomResourceArray[i]
+        }
+        if (randomResourceArray[i] === 'desert') {
+          desertIndex = i
+          //desertNumber = randomNumberArray[i]
+        }
+        randomFields.push({number: randomNumberArray[i], resource: randomResourceArray[i]})
+        if (i === randomNumberArray.length) {
+          // desertが入っている箇所に0を入れ、0が入っている箇所にdesertの数値を入れる
+          randomFields[nullIndex]['resource'] = nullResource
+          randomFields[desertIndex]['number'] = 0
+        }
+      }
+      return randomFields
+    }
+    const result = await proc(randomResourceArray, randomNumberArray)
+
+    return result
+  }
+
+
+
   return (
     <StyledRoot>
+      <button className="RegenerateButton" onClick={() => {clickRegenerateButton()}}>Regenerate Map</button>
+      { fields.length >= 1 &&
+      <React.Fragment>
       <div className="Column">
-        <AreaSVG />
-        <AreaSVG />
-        <AreaSVG />
+        <AreaSVG number={fields[0]['number']} resource={fields[0]['resource']} />
+        <AreaSVG number={fields[1]['number']} resource={fields[1]['resource']} />
+        <AreaSVG number={fields[2]['number']} resource={fields[2]['resource']} />
       </div>
       <div className="Column">
-        <AreaSVG />
-        <AreaSVG />
-        <AreaSVG />
-        <AreaSVG />
+        <AreaSVG number={fields[3]['number']} resource={fields[3]['resource']} />
+        <AreaSVG number={fields[4]['number']} resource={fields[4]['resource']} />
+        <AreaSVG number={fields[5]['number']} resource={fields[5]['resource']} />
+        <AreaSVG number={fields[6]['number']} resource={fields[6]['resource']} />
       </div>
       <div className="Column">
-        <AreaSVG />
-        <AreaSVG />
-        <AreaSVG />
-        <AreaSVG />
-        <AreaSVG />
+        <AreaSVG number={fields[7]['number']} resource={fields[7]['resource']} />
+        <AreaSVG number={fields[8]['number']} resource={fields[8]['resource']} />
+        <AreaSVG number={fields[9]['number']} resource={fields[9]['resource']} />
+        <AreaSVG number={fields[10]['number']} resource={fields[10]['resource']} />
+        <AreaSVG number={fields[11]['number']} resource={fields[11]['resource']} />
       </div>
+      </React.Fragment>
+      }
+      
     </StyledRoot>
   )
 }
@@ -35,6 +108,11 @@ const StyledRoot = styled.div`
   position: absolute;
   height: 100vh;
   width: 100vw;
+  z-index: 100;
+
+  .RegenerateButton {
+    /*z-index: 100;*/
+  }
   .Column {
     display: flex;
   }
